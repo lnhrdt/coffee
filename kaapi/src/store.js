670f156274b82b26e81494fdeeaab6b1d12527e7
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-05-05 16:21:48 -0700

    Add redux logging in dev builds

--- kaapi/src/store.js
+++ kaapi/src/store.js
@@ -1,8 +1,13 @@
 import {applyMiddleware, createStore} from 'redux'
 import thunk from 'redux-thunk'
+import {createLogger} from 'redux-logger'
 import rootReducer from './rootReducer'
 
 const middlewares = [thunk]
+if (process.env.NODE_ENV === 'development') {
+    const logger = createLogger()
+    middlewares.push(logger)
+}
 const store = applyMiddleware(...middlewares)(createStore)(rootReducer)
 
 export default store
