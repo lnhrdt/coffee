8833e3f5388211cde286f86f52cbcd1e6a139fb6
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-06-18 14:17:38 -0500

    Upgrade latte dependencies

--- latte/src/main/kotlin/io/leonhardt/coffee/latte/DataRepository.kt
+++ latte/src/main/kotlin/io/leonhardt/coffee/latte/DataRepository.kt
@@ -2,13 +2,14 @@ package io.leonhardt.coffee.latte
 
 import org.springframework.data.domain.Example
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 abstract class DataRepository<dataType, entityType, idType>(
         private val dataTranslator: Translator<dataType, entityType>,
         private val entityTranslator: Translator<entityType, dataType>,
         private val entityRepository: JpaRepository<entityType, idType>
 ) {
-    open fun findOne(input: Example<dataType>): dataType = entityRepository.findOne(Example.of(input.probe.toEntityType())).toDataType()
+    open fun findOne(input: Example<dataType>): Optional<dataType> = entityRepository.findOne(Example.of(input.probe.toEntityType())).map { it.toDataType() }
     open fun findAll(): List<dataType> = entityRepository.findAll().map { it.toDataType() }
 
     open fun save(input: dataType): dataType = entityRepository.save(input.toEntityType()).toDataType()
