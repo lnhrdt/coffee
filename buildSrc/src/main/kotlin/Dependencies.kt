e99f5c05d76e575e456a8fc7bf6f62e5760e3465
Leonhardt Koepsell <leonhardt@koepsell.io>
2019-03-15 20:48:35 -0700

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,11 @@
 object Versions {
     const val jvmTarget = "1.8"
-    const val kotlinVersion = "1.2.50"
-    const val springBootVersion = "2.0.3.RELEASE"
-    const val mockitoKotlinVersion = "2.0.0-alpha04"
+    const val kotlinVersion = "1.3.21"
+    const val springBootVersion = "2.1.3.RELEASE"
+    const val mockitoKotlinVersion = "2.1.0"
     const val arrowVersion = "0.7.2"
     const val beakVersion = "0.0.21"
 
-    const val node = "8.8.1"
-    const val yarn = "1.2.1"
+    const val node = "10.15.3"
+    const val yarn = "1.15.2"
 }
