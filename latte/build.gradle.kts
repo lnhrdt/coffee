6be0a9dbc758a955f8bdd556c8fc09107f00d258
Leonhardt Koepsell <leonhardt@koepsell.io>
2019-03-15 19:14:12 -0700

    Use the same versions object for all gradle scripts

---
+++ latte/build.gradle.kts
@@ -0,0 +1,7 @@
+apply {
+    from("../gradle/scripts/kotlin.gradle")
+    from("../gradle/scripts/springDependencyManagement.gradle")
+    from("../gradle/scripts/springBoot.gradle")
+    from("../gradle/scripts/persistence.gradle")
+    from("../gradle/scripts/arrow.gradle")
+}
